2181559d774670b2733efe6cf961ccb1fde93bad
Akshit Saxena <akshatsaxena102005@gmail.com>
2025-08-13 11:34:28 +0530

    initial commit to java fundamentals

---
+++ Assignment1.java
@@ -0,0 +1,119 @@
+
+// import java.util.*;
+
+// public class Assignment1 {
+
+    // public static void main(String[] args) {
+    //     System.out.println("Welcome to Bridgelabz!");
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("ENTER FIRST NUMBER");
+    //     int a = sc.nextInt();
+    //     System.out.println("ENTER SECOND NUMBER");
+    //     int b = sc.nextInt();
+    //     int sum = a + b;
+    //     System.out.println("THE SUM OF TWO NUMBERS IS -> "+sum);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("ENTER THE TEMP IN CELCIUS");
+    //     int celsius = sc.nextInt();
+    //     int Fahrenheit = (celsius * 9/5) + 32;
+    //     System.out.println("the temp in Ferhi will be -> " + Fahrenheit);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("ENTER THE radius");
+    //     int radius = sc.nextInt();
+    //     double Area = 3.14 * Math.pow(radius,2);
+    //     System.out.println("the area of circle will be -> " + Area);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("ENTER THE radius");
+    //     int radius = sc.nextInt();
+    //     System.out.println("ENTER THE height");
+    //     int height = sc.nextInt();
+    //     double Volume = 3.14 * Math.pow(radius,2)*height;
+    //     System.out.println("the area of circle will be -> " + Volume);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter Principal amount: ");
+    //     double principal = sc.nextDouble();
+
+    //     System.out.print("Enter Rate of interest: ");
+    //     double rate = sc.nextDouble();
+
+    //     System.out.print("Enter Time (in years): ");
+    //     double time = sc.nextDouble();
+
+    //     double simpleInterest = (principal * rate * time) / 100;
+
+    //     System.out.println("Simple Interest = " + simpleInterest);
+
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter the length of the rectangle: ");
+    //     double length = sc.nextDouble();
+
+    //     System.out.print("Enter the width of the rectangle: ");
+    //     double width = sc.nextDouble();
+
+    //     double perimeter = 2 * (length + width);
+
+    //     System.out.println("Perimeter of the rectangle = " + perimeter);
+
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter the base: ");
+    //     double base = sc.nextDouble();
+
+    //     System.out.print("Enter the exponent: ");
+    //     double exponent = sc.nextDouble();
+
+    //     double result = Math.pow(base, exponent);
+
+    //     System.out.println(base + " raised to the power " + exponent + " = " + result);
+
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter the first number: ");
+    //     double num1 = sc.nextDouble();
+
+    //     System.out.print("Enter the second number: ");
+    //     double num2 = sc.nextDouble();
+
+    //     System.out.print("Enter the third number: ");
+    //     double num3 = sc.nextDouble();
+
+    //     double average = (num1 + num2 + num3) / 3;
+
+    //     System.out.println("Average of the three numbers = " + average);
+    // }
+
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter distance in kilometers: ");
+//         double kilometers = sc.nextDouble();
+
+//         double miles = kilometers * 0.621371;
+
+//         System.out.println(kilometers + " kilometers = " + miles + " miles");
+
+//     }
+
+// }
